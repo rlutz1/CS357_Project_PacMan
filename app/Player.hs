d42734bbd3ef84b2f185ca824e07ab607ed42b2d
Roxanne Lutz <rlutz1@unm,edu>
2025-04-22 14:31:05 -0600

    commiting before i change some crap around with the movement

--- app/Player.hs
+++ app/Player.hs
@@ -19,55 +19,52 @@ genPlayer = Player playerStartPoint (Destination playerStartPoint, []) NONE NONE
 
 {-
 first, if the player currdir /= nextdir, check 
-  (1) if they're at the end of their track
+  if they're at close enough to end of their track
     grab the pivot (via player destination) from the board, try to grab the corresponding neighbor from that pivot
     if its null
       instead keep trying to go the same direction as before, can't turn there
       if that is null, stop moving -- keep same location
+want to clean this up, works well enough to continue testing, but there's def some gunk to cut out
 -}
 movePlayer :: Player -> Board -> Player
-movePlayer (Player loc (dest, []) curr next) b = (Player loc (dest, [loc]) curr next)
-movePlayer (Player loc (dest, t:ts) curr next) b
-  | closeEnough (ts) = queueTracks (Player t (dest, ts) curr next) b
-  | otherwise = Player t (dest, (ts)) curr curr
+movePlayer (Player loc (dest, []) curr next) b = Player loc (dest, [loc]) curr next
+movePlayer (Player _ (dest, t:ts) curr next) b
+  | closeEnough ts = queueTracks (Player t (dest, ts) curr next) b
+  | otherwise = Player t (dest, ts) curr curr
     
 queueTracks :: Player -> Board -> Player
-queueTracks (Player loc (dest, ts) curr next) b -- exactly one move left queued
+queueTracks (Player loc (dest, ts) curr next) b -- we are attempting oto queue up the next move
   | curr /= next = changeDir (Player loc (dest, ts) curr next) b 
   | otherwise = sameDir (Player loc (dest, ts) curr next) b 
 
--- movePlayer (Player loc (dest, [t]) curr next) b -- exactly one move left queued
---   | curr /= next = changeDir (Player loc (dest, [t]) curr next) b 
---   | otherwise = sameDir (Player loc (dest, [t]) curr next) b 
--- movePlayer (Player loc (dest, (t:ts)) curr next) _ = (Player t (dest, (ts)) curr curr)
-
+-- todo clean up
 changeDir :: Player -> Board -> Player
-changeDir (Player loc ((Destination point), ts) curr next) b 
-  | nextTracks == Null = sameDir (Player loc ((Destination point), ts) curr curr) b 
-  | otherwise = (Player loc (deconDestination nextTracks, ts ++ deconTracks nextTracks) next next)
+changeDir (Player loc (Destination point, ts) curr next) b 
+  | nextTracks == Null = sameDir (Player loc (Destination point, ts) curr curr) b 
+  | otherwise = Player loc (deconDestination nextTracks, ts ++ deconTracks nextTracks) next next
   where 
     nextPiv = getPivot point b
     nextTracks = getTracks nextPiv next
 
 
 
-
+-- todo clean up
 sameDir :: Player -> Board -> Player
-sameDir (Player loc ((Destination point), []) curr next) b = (Player loc ((Destination point), []) curr next)
-sameDir (Player loc ((Destination point), [t]) curr next) b 
-  | nextTracks == Null = Player loc (Destination loc, [t]) curr curr 
-  | otherwise = (Player loc (deconDestination nextTracks, [t] ++ deconTracks nextTracks) curr curr) 
+sameDir (Player loc (Destination point, []) curr _) _ = Player loc (Destination point, []) curr curr
+sameDir (Player loc (Destination point, [t]) curr _) b
+  | nextTracks == Null = Player loc (Destination point, [t]) curr curr 
+  | otherwise = Player loc (deconDestination nextTracks, t :deconTracks nextTracks) curr curr
   where 
     nextPiv = getPivot point b
     nextTracks = getTracks nextPiv curr
-sameDir (Player loc ((Destination point), ts) curr next) b = (Player loc ((Destination point), ts) curr next)
+sameDir (Player loc (Destination point, ts) curr next) _ = Player loc (Destination point, ts) curr next
 
 closeEnough :: [a] -> Bool
-closeEnough xs = length xs <=25
+closeEnough xs = length xs <= 25
 
 {-
 this is called every frame; todo:
 (1) change the position
 -}
 updatePlayer :: Player -> Board -> Player
-updatePlayer p b = movePlayer p b
+updatePlayer = movePlayer
